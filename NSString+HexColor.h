7f87185a84e9d3f81b3035641bf176b90ba85d09
Saurin <saurin.travadi@yahoo.com>
2013-05-29 22:15:49 -0500

    Change tint color for navigation bar, changed icon for guidedvideo, changed incomplete icon

---
+++ NSString+HexColor.h
@@ -0,0 +1,13 @@
+//
+//  NSString+HexColor.h
+//  GuidedVideo
+//
+//  Created by Saurin Travadi on 5/29/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString_HexColor : NSObject
+
+@end
---
+++ NSString+HexColor.m
@@ -0,0 +1,13 @@
+//
+//  NSString+HexColor.m
+//  GuidedVideo
+//
+//  Created by Saurin Travadi on 5/29/13.
+//
+//
+
+#import "NSString+HexColor.h"
+
+@implementation NSString_HexColor
+
+@end

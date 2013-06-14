34c854461a0602ae0d000bf83601192ec400f241
Saurin <saurin.travadi@yahoo.com>
2013-06-14 17:18:11 -0500

    Added tutorial class, defined constants for settings, made always randomized

---
+++ Classes/TutorialViewController.h
@@ -0,0 +1,13 @@
+//
+//  TutorialViewController.h
+//  GuidedVideo
+//
+//  Created by Saurin Travadi on 6/14/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TutorialViewController : UIViewController
+
+@end
---
+++ Classes/TutorialViewController.m
@@ -0,0 +1,38 @@
+//
+//  TutorialViewController.m
+//  GuidedVideo
+//
+//  Created by Saurin Travadi on 6/14/13.
+//
+//
+
+#import "TutorialViewController.h"
+
+@interface TutorialViewController ()
+
+@end
+
+@implementation TutorialViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

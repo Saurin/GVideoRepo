91d77f1f92d445128ca56887a2bf44c684e71d55
Saurin <saurin.travadi@yahoo.com>
2013-06-24 20:45:25 -0500

    Disabling NEXT button on Instruction View

---
+++ Classes/Message.h
@@ -0,0 +1,13 @@
+//
+//  Message.h
+//  GuidedVideo
+//
+//  Created by Saurin Travadi on 6/23/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Message : UIAlertView
+
+@end
---
+++ Classes/Message.m
@@ -0,0 +1,31 @@
+//
+//  Message.m
+//  GuidedVideo
+//
+//  Created by Saurin Travadi on 6/23/13.
+//
+//
+
+#import "Message.h"
+
+@implementation Message
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
